f3628ebbd5823a01348c5bac56a40586ae015627
SET001 <settydark@gmail.com>
2016-04-30 16:09:49 +0300

    Updated to version 0.1.4

--- tasks/release.js
+++ tasks/release.js
@@ -7,13 +7,14 @@ var errorHandler = function(error){
 
 module.exports = function(gulp){
 	return function(done){
+		console.log("Ad");
 		gulp.src("./")
-			.pipe(git.commit("Updated to version " + util.env.version, {args: "--all"}), errorHandler)
-			// .pipe(git.checkout('master'), errorHandler)
+			.pipe(git.commit("Updated to version " + util.env.version, {args: "--all"}, errorHandler))
+			// .pipe(git.checkout('master', errorHandler))
+			;
 			// .pipe(git.merge('dev'), errorHandler)
 			// .pipe(git.checkout('dev'), errorHandler)
 			// .pipe(git.push('origin', 'master'), errorHandler)
-			.on('error', errorHandler);
 		done();
 	}
 }
